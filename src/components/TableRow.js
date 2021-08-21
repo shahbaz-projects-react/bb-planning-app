9f8e40566cfb963d1994b622f4f19a672e382cf5
Aditi Bose <aditi.bose@citrix.com>
2021-08-21 16:24:04 +0530

    Style update Table Row

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,9 +1,22 @@
 import React from "react";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme)=>({
+  firstLetter: {
+    '&:first-letter':{
+      fontWeight:'bold',
+      fontSize:theme.spacing(2),
+      color:'#da0505d4'
+    }
+  }
+}));
+
 
 export const DataRow = ({ row, day }) => {
   const { Lunch, Dinner } = row;
+  const classes = useStyles();
   return (
     <>
       <TableRow key={row.name}>
@@ -13,12 +26,12 @@ export const DataRow = ({ row, day }) => {
         {}
         <TableCell align="center">{"Lunch"}</TableCell>
         <TableCell align="center">{Lunch?.food || "-"}</TableCell>
-        <TableCell align="center">{Lunch?.person || "-"}</TableCell>
+        <TableCell align="center" className={classes.firstLetter}>{Lunch?.person || "-"}</TableCell>
       </TableRow>
       <TableRow key={row.name}>
         <TableCell align="center">{"Dinner"}</TableCell>
         <TableCell align="center">{Dinner?.food || "-"}</TableCell>
-        <TableCell align="center">{Dinner?.person || "-"}</TableCell>
+        <TableCell align="center" className={classes.firstLetter}>{Dinner?.person || "-"}</TableCell>
       </TableRow>
     </>
   );
