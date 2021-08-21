87d91f7ef9e6c53b154f84c5cbc50f1af8504742
Aditi Bose <aditi.bose@citrix.com>
2021-08-21 19:33:26 +0530

    Update for Heroku deployment issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MainContainer } from "./components/Container";
+import { MainContainer } from "./components/MainContainer";
 import { store,StateProvider } from "./redux/store";
 import Typography from "@material-ui/core/Typography";
 import Inputs from "./components/Inputs";
